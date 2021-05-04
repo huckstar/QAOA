304244a5afe59713673bcc82e67dc3faed5aac93
huckstar <kxhuang@umd.edu>
2021-05-04 13:33:43 -0400

    Update qaoa_sv.py

--- qaoa_sv.py
+++ qaoa_sv.py
@@ -15,23 +15,25 @@ from scipy.optimize import minimize
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
-n = 6   #set paramters of max -k-vertex
-k = 3
+n = 10   #set paramters of max -k-vertex
+k = 5
 
-num_graph = 20
+num_graph = 2
 mixer = 1 #1:complete; 2: ring; 3: Grover
-max_layer = 5
+max_layer = 2
 
 backend = Aer.get_backend('statevector_simulator')
 ################
 average_ratio1=[]
 average_ratio2=[]
+average_ratio3=[]
 average_depth1=[]
 average_depth2=[]
+average_depth3=[]
 for layer in range(max_layer):
-    ratio={'1':[],'2':[]}
+    ratio={'1':[],'2':[],'3':[]}
     
-    depth={'1':[],'2':[]}
+    depth={'1':[],'2':[],'3':[]}
     
     for idex in range(num_graph):
 
@@ -39,13 +41,13 @@ for layer in range(max_layer):
         #get the circuit
         #qc.draw(output='mpl',filename='my_circuit2.png')
         p = layer+1
-        for mixer in (1,2):
+        for mixer in (1,2,3):
 
             obj = get_black_box_objective_sv(G,p,n,k,mixer) #optimized target
         
 
 
-            init_point = np.ones((2*p,), dtype=int)
+            init_point = np.zeros((2*p,), dtype=int)
             res_sample = minimize(obj,init_point,method='COBYLA',options={'maxiter':2500,'disp':True})
         
 
@@ -70,16 +72,20 @@ for layer in range(max_layer):
     average_depth1.append(np.mean(depth['1']))
     average_ratio2.append(np.mean(ratio['2']))
     average_depth2.append(np.mean(depth['2']))
+    average_ratio3.append(np.mean(ratio['3']))
+    average_depth3.append(np.mean(depth['3']))
 
 
 fig, axs = plt.subplots(2)
 axs[0].plot(range(max_layer),average_ratio1, label = 'complete')
 axs[0].plot(range(max_layer),average_ratio2, label = 'ring')
+axs[0].plot(range(max_layer),average_ratio3, label = 'Grover')
 axs[0].set(xlabel = 'layer',ylabel = 'ratio')
 axs[0].legend()
 axs[1].plot(average_depth1,average_ratio1, label = 'complete')
 axs[1].plot(average_depth2,average_ratio2, label = 'ring')
+axs[1].plot(average_depth3,average_ratio3, label = 'Grover')
 axs[1].set(xlabel = 'depth',ylabel = 'ratio')
 axs[1].legend()
 plt.show()
-plt.savefig('test4.png')
+plt.savefig('test_num_10_4.png')
